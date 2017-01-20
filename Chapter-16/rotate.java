5f52735cece43387b313c72d419a80d9b08361ed
Chris Saechao <chris_saechao@live.com>
2017-01-20 14:06:25 -0800

    Create rotate.java

---
+++ Chapter-16/rotate.java
@@ -0,0 +1,34 @@
+/*
+Write a method rotate that moves the value at the front of a list of integers to the end of the list.
+For example, if a variable called list stores the following sequence of values:
+
+[8, 23, 19, 7, 45, 98, 102, 4]
+Then the call of list.rotate(); should move the value 8 from the front of the list to the back of the list, 
+yielding this sequence of values:
+
+[23, 19, 7, 45, 98, 102, 4, 8]
+The other values in the list should retain the same order as in the original list. If the method is 
+called for a list of 0 or 1 elements it should have no effect on the list. 
+You are not allowed to construct any new nodes to solve this problem and you are not allowed to change any of the 
+integer values stored in the nodes. You must solve the problem by rearranging the links of the list.
+
+Assume that you are adding this method to the LinkedIntList class as defined below:
+*/
+public class LinkedIntList {
+    private ListNode front;   // null for an empty list
+    ...
+}
+
+
+public void rotate() {
+    if(this.front != null && this.front.next != null) {
+        ListNode list2 = this.front;
+        front = front.next;
+        ListNode temp = front;
+        while(temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = list2;
+        list2.next = null;
+    }
+}
